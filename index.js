c9f6d0e92b3e21e0a3ce28d7ac5175fe5351f383
Aman Jaiswal <general@amanjaiswal.vip>
2024-03-02 22:54:04 -0500

    Content Finished

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+// Repeat for other HTML files as needed
+
+
+// Catch-all route to handle user entering any other route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}/`);
+});
